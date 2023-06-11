fb943b06286c5c8df41a6c97efd53a801006dbd6
raburebure <oscar.rabure@gmail.com>
2023-06-11 15:00:43 +0900

    2nd commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const authRoute = require("./routers/auth");
+const postsRoute = require("./routers/posts");
+const usersRoute = require("./routers/users");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/users", usersRoute);
+
+// app.get("/", (req,res) => {
+//     res.send("<h1>Hello</h1>");
+// })
+
+app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));
---
+++ utils/generateIdenticon.js
@@ -0,0 +1,12 @@
+const Identicon = require("identicon.js");
+
+const generateIdenticon = (input, size = 64) => {
+  // 入力項目をハッシュ化
+  const hash = require("crypto").createHash("md5").update(input).digest("hex");
+
+  const data = new Identicon(hash, size).toString();
+
+  return `data:image/png;base64,${data}`;
+};
+
+module.exports = generateIdenticon;
